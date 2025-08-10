2bf2a115dc5e91c6602c13372b2edf6efb42153d
jimchen5209 <jimchen5209@gmail.com>
2025-08-10 20:58:01 +0800

    feat: app bar and about page

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,32 +1,5 @@
-<script setup lang="ts">
-const items = [
-  {
-    title: 'GitHub',
-    icon: `mdi-github`,
-    href: 'https://github.com/LCE-Lab/Analysis_Bot_Web',
-  },
-  {
-    title: 'GitHub (BackEnd)',
-    icon: `mdi-github`,
-    href: 'https://github.com/LCE-Lab/Analysis_Bot',
-  },
-]
-</script>
-
 <template>
   <v-footer app height="40">
-    <a
-      v-for="item in items"
-      :key="item.title"
-      class="d-inline-block mx-2 social-link"
-      :href="item.href"
-      rel="noopener noreferrer"
-      target="_blank"
-      :title="item.title"
-    >
-      <v-icon :icon="item.icon" :size="item.icon === '$vuetify' ? 24 : 16" />
-    </a>
-
     <div class="text-caption text-disabled" style="position: absolute; right: 16px">
       &copy; 2020-{{ new Date().getFullYear() }}
       <span class="d-none d-sm-inline-block">LCE-Lab</span>
--- src/layouts/defaultLayout.vue
+++ src/layouts/defaultLayout.vue
@@ -1,6 +1,17 @@
 <template>
+  <v-app-bar :elevation="2">
+    <v-app-bar-title text="Discord Online Analyzer" />
+
+    <template v-slot:append>
+      <v-btn to="/" text="Home" />
+      <v-btn to="/about" text="About" />
+    </template>
+  </v-app-bar>
+
   <v-main>
-    <router-view />
+    <v-container class="fill-height" max-width="900">
+      <router-view />
+    </v-container>
   </v-main>
 
   <AppFooter />
