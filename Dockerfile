826e01d802968c7a891afa336fddfdc2e7007b23
jimchen5209 <jimchen5209@gmail.com>
2025-08-10 15:07:29 +0800

    refactor: re-init project with vue3+vuetify

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,15 @@
-FROM node:20 as build-stage
+FROM node:22 as build-stage
 WORKDIR /app
-COPY package*.json ./
-RUN npm install
-COPY ./ .
-RUN npm run build
+
+ENV NODE_ENV=production
+ENV COREPACK_ENABLE_DOWNLOAD_PROMPT=0
+
+RUN corepack enable
+
+COPY . .
+RUN yarn install --immutable
+
+RUN yarn build
 RUN cp /app/cfg/settings.js.example /app/cfg/settings.js
 
 FROM nginx:alpine-slim as production-stage
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,36 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import day from '../components/day.vue'
-import week from '../components/week.vue'
-import custom from '../components/custom.vue'
-import index from '../components/index.vue'
+/**
+ * router/index.ts
+ *
+ * Automatic routes for `./src/pages/*.vue`
+ */
 
-Vue.use(Router)
+// Composables
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { setupLayouts } from 'virtual:generated-layouts'
+import { routes } from 'vue-router/auto-routes'
 
-export default new Router({
-  routes: [{
-    path: '/',
-    name: 'index',
-    component: index
-  }, {
-    path: '/day/:serverID',
-    name: 'day',
-    component: day
-  }, {
-    path: '/week/:serverID',
-    name: 'week',
-    component: week
-  }, {
-    path: '/custom/:serverID',
-    name: 'custom',
-    component: custom,
-    props: route => ({ start: route.query.start, end: route.query.end })
-  }]
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: setupLayouts(routes),
 })
+
+// Workaround for https://github.com/vitejs/vite/issues/11804
+router.onError((err, to) => {
+  if (err?.message?.includes?.('Failed to fetch dynamically imported module')) {
+    if (localStorage.getItem('vuetify:dynamic-reload')) {
+      console.error('Dynamic import error, reloading page did not fix it', err)
+    } else {
+      console.log('Reloading page to fix dynamic import error')
+      localStorage.setItem('vuetify:dynamic-reload', 'true')
+      location.assign(to.fullPath)
+    }
+  } else {
+    console.error(err)
+  }
+})
+
+router.isReady().then(() => {
+  localStorage.removeItem('vuetify:dynamic-reload')
+})
+
+export default router
